8236b00aa482ee088a4031f0eef70ec3b06461c8
Mathieu Simon <mathieu.simon@simweb.ch>
2011-04-07 23:54:39 +0200

    Import Skeleton from 'Serie 3'

---
+++ serie3/compile.c
@@ -0,0 +1,19 @@
+/* TODO: Task (b) Please fill in the following lines, then remove this line.
+ *
+ * author(s):   FIRSTNAME LASTNAME 
+ *              (FIRSTNAME2 LASTNAME2)
+ * modified:    2010-01-07
+ *
+ */
+
+#include <stdlib.h>
+#include <stdio.h>
+#include "memory.h"
+#include "mips.h"
+#include "compiler.h"
+ 
+int main ( int argc, char** argv ) {
+    /* TODO: Task (c) implement main */
+    return EXIT_SUCCESS;
+}
+
