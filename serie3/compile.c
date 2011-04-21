bd0429abde6fe01dad46e9851b906f34891296fd
Mathieu Simon <mathieu.simon@simweb.ch>
2011-04-21 10:11:26 +0200

    Hmm Input check ok, but not yet launching compiler

--- serie3/compile.c
+++ serie3/compile.c
@@ -1,8 +1,7 @@
-/* TODO: Task (b) Please fill in the following lines, then remove this line.
- *
- * author(s):   FIRSTNAME LASTNAME 
- *              (FIRSTNAME2 LASTNAME2)
- * modified:    2010-01-07
+/* 
+ * author(s):   Mathieu Simon
+ *              Thomas Rickenbach
+ * modified:    2011-04-20
  *
  */
 
@@ -13,7 +12,25 @@
 #include "compiler.h"
  
 int main ( int argc, char** argv ) {
-    /* TODO: Task (c) implement main */
-    return EXIT_SUCCESS;
+   	int i;
+	int count;
+	count = argc;
+	count = count-1;
+
+	if (count<2){
+		printf("usage: <commandName> expression filename\n");
+		return EXIT_FAILURE;
+	}
+
+	if (count>2){
+		printf("usage: <commandName> expression filename\n");
+		return EXIT_FAILURE;
+	}
+
+	else {
+		printf("Input: %s\n", argv[1]);
+		//	system(./compile/* TODO: Task (c) implement main */
+		return EXIT_SUCCESS;
+	}
 }
 
